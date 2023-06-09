f262719a2a584dfc1fb3a5d3ff9d6c8ba463154a
Victor <victorribera92@gmail.com>
2023-06-09 19:20:55 +0200

    fix locale selector on reload

--- src/common/components/navbar/LocaleSelector.tsx
+++ src/common/components/navbar/LocaleSelector.tsx
@@ -1,8 +1,16 @@
 import styles from "./LocaleSelector.module.scss";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function LocaleSelector() {
   const router = useRouter();
+
+  useEffect(() => {
+    const checkbox = document.getElementById("language-toggle") as HTMLInputElement;
+    if (checkbox) {
+      router.locale === "en" ? checkbox.checked = false : checkbox.checked = true
+    }
+  });
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     if (e.target.checked) {
